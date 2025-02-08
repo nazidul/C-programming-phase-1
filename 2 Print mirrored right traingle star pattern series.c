003e6d21b6518a4ef21311fba222f74f157917c8
MD NAZIDUL ISLAM <132178832+nazidul@users.noreply.github.com>
2025-02-08 13:28:30 +0600

    Add files via upload

---
+++ 2 Print mirrored right traingle star pattern series.c
@@ -0,0 +1,29 @@
+//library function
+#include<stdio.h>
+//main function
+int main()
+
+{
+    //data-type and variable declare
+    int i, j, rows;
+    //Input rows from user
+    printf("Enter number of rows:");
+    scanf("%d",&rows);
+    //iterate through rows
+    for(i=1;i<=rows;i++)
+    {
+        //print space in decreasing order of rows
+        for(j=1;j<=rows;j++)
+            {
+                printf(" ");
+            }
+            //print star in increasing order of rows
+    for(j=1;j<=i;j++)
+    {
+        printf("*");
+    }
+    //move to next line
+    printf("\n");
+    }
+    return 0;
+}
